f13f33cd3a5289b917244272bfd506176ff3fa86
Ryan <rahaack@iastate.edu>
2014-03-08 17:38:59 -0600

    UI example in main

--- src/edu/iastate/cs362/project/Main.java
+++ src/edu/iastate/cs362/project/Main.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 
+import javax.swing.JOptionPane;
+
 public class Main implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -12,6 +14,22 @@ public class Main implements Serializable{
 	{
 		File file = new File("1dad");
 		BankController test = new BankController(file);
+		
+		
+		String userID =JOptionPane.showInputDialog(
+			    "Enter user ID");
+		
+		String password =JOptionPane.showInputDialog(
+			    "Enter password");
+		
+		
+		String operationNumber=JOptionPane.showInputDialog(
+				"Logged in as 'staff or user' \n"+
+			    "Enter operation number \n"+
+			    " 1. Create User\n"+
+			    " 2. Create Account\n"+
+			    " 3. Manage Accounts");
+		int code = Integer.parseInt(operationNumber);
 		
 		while(true)
 		{
