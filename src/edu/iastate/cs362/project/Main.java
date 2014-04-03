10bc96ab60f8e60911def9db92714fef99f56a4a
Garrett Phelps <gdphelps@iastate.edu>
2014-04-03 00:02:25 -0500

    Added the UI. It might still be missing some features but you are able to login and create account. Also there is a stub for adding accounts to the User but there is no API call. Which brings up the other point that the some API calls I make might be broken because the API isnt updated yet.

--- src/edu/iastate/cs362/project/Main.java
+++ src/edu/iastate/cs362/project/Main.java
@@ -4,25 +4,17 @@ import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 
+import javax.swing.JOptionPane;
+
 public class Main implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	public static BankController controller;
 
 	public static void main(String arg[]) throws IOException {
-
+		//MainFrame frame = new MainFrame();
 		File file = new File("Database.bankdata");
-		BankController test = new BankController(file);
-		test.translate();
-//		String userID = JOptionPane.showInputDialog("Enter user ID");
-//
-//		String password = JOptionPane.showInputDialog("Enter password");
-//
-//		String operationNumber = JOptionPane
-//				.showInputDialog("Logged in as 'staff or user' \n"
-//						+ "Enter operation number \n" + " 1. Create User\n"
-//						+ " 2. Create Account\n" + " 3. Manage Accounts");
-//		int code = Integer.parseInt(operationNumber);
-
+		controller = new BankController(file);
+		MainFrame.startApp();
 	}
-
 }
